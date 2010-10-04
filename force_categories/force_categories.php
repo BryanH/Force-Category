f1a4cbdc06dfc9c57c3ff7af7332a135ffd9b54e
BryanH <bryanh41@hotmail.com>
2010-10-04 14:12:13 -0500

    Added menu to user screen - ugly and unformated for now

--- force_categories/force_categories.php
+++ force_categories/force_categories.php
@@ -35,11 +35,11 @@ if (!class_exists("ForceCategories")) {
 				'classname' => 'force_categories',
 				'description' => __('Forces user posts to include and/or exclude specified categories', 'force_categories')
 			);
-			$control_ops = array (
-				'width' => 300,
-				'height' => 350,
-				'id_base' => 'force_categories-plugin'
-			);
+//			$control_ops = array (
+//				'width' => 300,
+//				'height' => 350,
+//				'id_base' => 'force_categories-plugin'
+//			);
 			add_action('plugins_loaded', array (
 				$this,
 				'register_plugin'
@@ -71,80 +71,77 @@ if (!class_exists("ForceCategories")) {
 				);
 			register_setting($this->plugin->dom, $this->plugin->dom, $callback);
 		}
-	}
-	/*
-		* Retrieves the value of the key from a form's values
-		* Updates the datastore with that key:value
-		* Parameter: 		$key - form and datastore key (must be identical)
-		* Returns: 		value passed so the intermediate variable can be updated
+		/*
+			* Retrieves the value of the key from a form's values
+			* Updates the datastore with that key:value
+			* Parameter: 		$key - form and datastore key (must be identical)
+			* Returns: 		value passed so the intermediate variable can be updated
+			*/
+		function update_usermeta_from_post($key) {
+			if (true == empty ($key)) {
+				wp_die(__('Invalid key passed to fc_update_from_post'));
+			}
+			update_usermeta($key, $_POST[$key]);
+			return $_POST[$key];
+		}
+		/*
+		* Retrieves value from datastore. If nothing returned,
+		* then it returns the default (or null if no default)
+		* (equivalent to $foo = $bar || $default)
+		* Parameters:	$key - datastore key
+		*				$default - default value
+		* Returns: either datastore's value, or default if former is empty
 		*/
-	function update_from_post($key) {
-		if (true == empty ($key)) {
-			wp_die(__('Invalid key passed to fc_update_from_post'));
+		function get_value_or_default($key, $default = null) {
+			$the_data = get_option($key);
+			if (true == empty ($the_data)) {
+				$the_data = $default;
+			}
+			return $the_data;
 		}
-		update_option($key, $_POST[$key]);
-		return $_POST[$key];
-	}
-	/*
-	* Retrieves value from datastore. If nothing returned,
-	* then it returns the default (or null if no default)
-	* (equivalent to $foo = $bar || $default)
-	* Parameters:	$key - datastore key
-	*				$default - default value
-	* Returns: either datastore's value, or default if former is empty
-	*/
-	function get_value_or_default($key, $default = null) {
-		$the_data = get_option($key);
-		if (true == empty ($the_data)) {
-			$the_data = $default;
-		}
-		return $the_data;
-	}
-	/*
-	 * Admin Option menu
-	 */
-	function fc_plugin_menu() {
-		add_options_page(__('"Force Categories" Options', 'menu-fc'), __('Force Categories', 'menu-fc'), 'manage_options', 'forcecatsettings', array (
-			& $this,
-			'fc_options'
-		));
-	}
-	/*
-	 * Option screen
-	 */
-	function fc_options() {
-		if (!current_user_can('manage_options')) {
-			wp_die(__('You do not have sufficient permissions to access this page.'));
-		}
-		$social_media = $this->get_value_or_default($this->meta_fc, $this->default_meta_fc);
-		$hidden_field_name = 'fc_submit_hidden';
-		if (isset ($_POST[$hidden_field_name]) && 'Y' == $_POST[$hidden_field_name]) {
-			/* variables for the field and option names */
-			// TODO: update using array
-			$opt_max_words = $this->update_from_post($this->meta_fc);
+		/*
+		 * Get the styles installed
+		 */
+function fc_style_enqueue(){
+			$css_source = $this->get_css_location("force_cat.css");
+			wp_enqueue_style($this->plugin->dom, "/wp-content/plugins/force_categories/stylesheets/force_cat.css");//$css_source );
+			wp_print_styles( $this->plugin->dom );
+}
+		/*
+		 * Option screen
+		 */
+		function fc_options($user) {
+			if (!current_user_can('edit_user')) {
+				wp_die(__('You do not have sufficient permissions to access this page.'));
+			}
+
 ?>
-<div class="updated"><p><strong><?php _e('settings saved', 'menu-fc' ); ?></strong></p></div>
+<?php include(WP_PLUGIN_DIR . '/' . str_replace(basename(__FILE__), "", plugin_basename(__FILE__)) . "options.php"); ?>
+
+<?php return; ?>
 <?php
 
+			return;
+			add_action('personal_options_update', 'save_fc_options');
+			add_action('edit_user_profile_update', 'save_fc_options');
 		}
-?>
-<?php include("options.php"); ?>
-<?php
-
-	}
-	/*
-	 * Obtains the url and file location of a given CSS
-	 * Parameter:	css filename (assumes it lives in the 'stylesheets' directory under the plugin)
-	 * Output: 		file, url to stylesheet.
-	 * Use:			list($cssfile, $cssurl) = get_css_location('somecss.css');
-	 */
-	function get_css_location($css) {
-		$admin_css = '/' . str_replace(basename(__FILE__), "", plugin_basename(__FILE__)) . 'stylesheets/' . $css;
-		$css_location = array (
-			WP_PLUGIN_DIR . $admin_css,
-			WP_PLUGIN_URL . $admin_css
-		);
-		return $css_location;
+		function save_fc_options($user_id) {
+			update_usermeta_from_post('field1');
+		}
+		/*
+		 * Obtains the url and file location of a given CSS
+		 * Parameter:	css filename (assumes it lives in the 'stylesheets' directory under the plugin)
+		 * Output: 		file, url to stylesheet.
+		 * Use:			list($cssfile, $cssurl) = get_css_location('somecss.css');
+		 */
+		function get_css_location($css) {
+			$admin_css = '/' . str_replace(basename(__FILE__), "", plugin_basename(__FILE__)) . 'stylesheets/' . $css;
+			$css_location =
+				WP_PLUGIN_DIR . $admin_css
+//				WP_PLUGIN_URL . $admin_css
+;
+			return $css_location;
+		}
 	}
 }
 if (class_exists("ForceCategories")) {
@@ -153,14 +150,19 @@ if (class_exists("ForceCategories")) {
 if (isset ($force_cats)) {
 	// TODO: Actions here
 	if (is_admin()) { // admin actions
-		add_action('admin_menu', array (
+		add_action('edit_user_profile', array (
 			& $force_cats,
-			'force_cats_menu'
+			'fc_options'
 		));
-		add_action('admin_init', array (
+		add_action('show_user_profile', array (
 			& $force_cats,
-			'register_mysettings'
+			'fc_options'
 		));
+		add_action('admin_init', array( & $force_cats, 'fc_style_enqueue' ));
+		//		add_action('admin_init', array (
+		//			& $force_cats,
+		//			'register_mysettings'
+		//		));
 	} else {
 		// non-admin enqueues, actions, and filters
 	}
---
+++ force_categories/options.php
@@ -0,0 +1,30 @@
+<h3>'Force Categories' Settings</h3>
+
+<p><span class="dropcap">M</span><strong>ust have</strong> categories will be assigned to every post by this user, whereas <strong>can&#8217;t have</strong> categories can never be assigned to any posts by this user.</p>
+<div id="catres"><!--Spacer needed to anchor the parent-->
+<div class="spacer"></div>
+<div id="musthave" class="catpick">
+<h2>Must have</h2>
+<select class="catselect" name="musthave" size="6" multiple="multiple">
+<option>123</option>
+<option>34242</option>
+</select></div>
+<div id="mustactions" class="catactions"><input type="button" name="add_must" value="&#171;" /><br />
+<input type="button" name="remove_must" value="&#187;" /></div>
+<div id="categories" class="catpick">
+<h2>Categories</h2>
+<select class="catselect" name="categories" size="6" multiple="multiple">
+<option>123</option>
+<option>34242</option>
+</select></div>
+<div id="cantactions" class="catactions"><input type="button" name="add_cant" value="&#171;" /><br />
+<input type="button" name="remove_cant" value="&#187;" /></div>
+<div id="canthave" class="catpick">
+<h2>Can't have</h2>
+<select class="catselect" name="canthave" size="6" multiple="multiple">
+<option>123</option>
+<option>34242</option>
+</select></div>
+<!--Spacer needed to anchor the parent-->
+<div class="spacer"></div>
+</div>
